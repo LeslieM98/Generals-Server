abf943e00655c49b018920b8bf11863c81f3b86d
leslie <2007732+LeslieM98@users.noreply.github.com>
2019-09-30 22:26:05 +0200

    initial commit

---
+++ src/main/java/me/leslie/generals/dedicated/server/App.java
@@ -0,0 +1,14 @@
+package me.leslie.generals.dedicated.server;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        new App().start(args);
+    }
+
+    public void start(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
