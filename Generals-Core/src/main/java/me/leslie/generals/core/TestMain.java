e00383803ee121ced3359763355fa93059f2f60c
leslie <2007732+LeslieM98@users.noreply.github.com>
2019-10-12 02:25:38 +0200

    restructured project and set java versions

---
+++ Generals-Core/src/main/java/me/leslie/generals/core/TestMain.java
@@ -0,0 +1,7 @@
+package me.leslie.generals.core;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println(new Color(25, 122, 99));
+    }
+}
---
+++ Generals-Server/src/main/java/me/leslie/generals/server/App.java
@@ -0,0 +1,10 @@
+package me.leslie.generals.server;
+
+import me.leslie.generals.core.Color;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println(new Color(124, 99, 234));
+    }
+}
