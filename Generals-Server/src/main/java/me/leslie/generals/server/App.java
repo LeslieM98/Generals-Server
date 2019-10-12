607e5769693df3a7988461f9795dc03af065a69c
leslie <2007732+LeslieM98@users.noreply.github.com>
2019-10-12 16:33:02 +0200

    added client submodule

--- Generals-Server/src/main/java/me/leslie/generals/server/App.java
+++ Generals-Server/src/main/java/me/leslie/generals/server/App.java
@@ -5,6 +5,6 @@ import me.leslie.generals.core.Color;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println(new Color(124, 99, 234));
+        System.out.println("Hello World!");
     }
 }
