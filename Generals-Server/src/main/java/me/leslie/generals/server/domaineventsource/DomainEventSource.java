f0b2a12d2ee39ecde56085d55dd46d5ac44594a6
Leslie M <2007732+LeslieM98@users.noreply.github.com>
2019-11-17 17:22:29 +0100

    Removed domaineventsource

--- Generals-Server/src/main/java/me/leslie/generals/server/domaineventsource/DomainEventSource.java
+++
@@ -1,7 +0,0 @@
-package me.leslie.generals.server.domaineventsource;
-
-import me.leslie.generals.core.domainevent.DomainEvent;
-
-public interface DomainEventSource {
-    DomainEvent getNext();
-}
