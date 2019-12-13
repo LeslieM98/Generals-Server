fc0a8c996c1b7e9d074fa6efc0927c6bab070c5f
leslie <2007732+LeslieM98@users.noreply.github.com>
2019-12-13 16:28:39 +0100

    Army only consists of IDs now

--- src/main/java/me/leslie/generals/server/valueobject/Army.java
+++ src/main/java/me/leslie/generals/server/valueobject/Army.java
@@ -1,12 +1,11 @@
 package me.leslie.generals.server.valueobject;
 
 import lombok.Value;
-import me.leslie.generals.server.model.Troop;
 
 import java.util.Set;
 
 @Value
 public class Army {
-    Troop hq;
-    Set<Troop> troops;
+    TroopID hq;
+    Set<TroopID> troops;
 }
