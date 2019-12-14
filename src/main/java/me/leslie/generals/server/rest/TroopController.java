3e52add193df5d3bcceb5c66696a27a27a26348f
leslie <2007732+LeslieM98@users.noreply.github.com>
2019-12-14 02:12:20 +0100

    basic CRUD implementation for TroopController

--- src/main/java/me/leslie/generals/server/rest/TroopController.java
+++ src/main/java/me/leslie/generals/server/rest/TroopController.java
@@ -1,40 +1,64 @@
 package me.leslie.generals.server.rest;
 
+import com.google.gson.Gson;
 import me.leslie.generals.server.model.Troop;
 import me.leslie.generals.server.repository.TroopRepository;
-import me.leslie.generals.server.valueobject.*;
+import me.leslie.generals.server.valueobject.TroopID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/troop/*")
 public class TroopController {
     private final TroopRepository troopRepository;
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    public TroopController(TroopRepository troopRepository){
+    public TroopController(TroopRepository troopRepository) {
         this.troopRepository = troopRepository;
     }
 
-    @GetMapping(value = "create")
-    public Troop create() {
-        Troop t = new Troop(new TroopID(1), 1, 1, new Vector2D(1.0, 1.0), new MovementSpeed(1.0, 1.0, 1.0), new CombatRange(1.0, 1.0), new ViewDistance(1.0, 1.0, 1.0));
-        troopRepository.save(t);
-        return t;
+    @PutMapping(value = "save")
+    public ResponseEntity<String> save(@RequestBody String json) {
+        Troop troop = new Gson().fromJson(json, Troop.class);
+        troopRepository.save(troop);
+        logger.info("Save request for: {}", troop);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping(value = "delete")
-    public Troop delete(){
-        Troop t = new Troop(new TroopID(1), 1, 1, new Vector2D(1.0, 1.0), new MovementSpeed(1.0, 1.0, 1.0), new CombatRange(1.0, 1.0), new ViewDistance(1.0, 1.0, 1.0));
-        troopRepository.delete(t);
-        return t;
+    public ResponseEntity<Troop> delete(@RequestBody String json) {
+        TroopID troopID = new Gson().fromJson(json, TroopID.class);
+        Optional<Troop> troop = troopRepository.findById(troopID);
+        if (troop.isEmpty()) {
+            logger.info("Delete request not successful for: {}", troopID);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        troopRepository.deleteById(troopID);
+        logger.info("Delete request successful for: {}", troopID);
+        return new ResponseEntity<>(troop.get(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "getall")
+    public Iterable<Troop> getAll() {
+        return troopRepository.findAll();
     }
 
     @GetMapping(value = "get")
-    public Iterable<Troop> get(){
-        return troopRepository.findAll();
+    public ResponseEntity<Troop> get(@RequestParam int id) {
+        TroopID troopID = new TroopID(id);
+        Optional<Troop> troop = troopRepository.findById(troopID);
+        if (troop.isEmpty()) {
+            logger.info("Get request not successful for: {}", troopID);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        logger.info("Get request successful for: {}", troopID);
+        return new ResponseEntity<>(troop.get(), HttpStatus.OK);
     }
 }
