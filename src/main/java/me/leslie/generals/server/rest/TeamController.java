f735af1a2fa401769d533c8a1c5a651bb0e2649d
OoTryItoO <simon.j.becker@web.de>
2020-01-02 23:02:43 +0100

    TeamID started, GetMapping (get&getall) finished, needs cleanup @Autowired

--- src/main/java/me/leslie/generals/server/rest/TeamController.java
+++ src/main/java/me/leslie/generals/server/rest/TeamController.java
@@ -4,36 +4,45 @@ import com.google.gson.Gson;
 import me.leslie.generals.server.model.gameentity.Team;
 import me.leslie.generals.server.repository.gameentity.ITeamRepository;
 import me.leslie.generals.server.valueobject.TeamID;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Optional;
-import java.util.logging.Logger;
+
 
 public class TeamController {
 
-    private final TeamID teamId;
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final ITeamRepository teamRepository;
-    private final org.slf4j.Logger logger = LoggerFactory.getLogger(getClass());
+
+    //TODO: Cleanup
+    @Autowired
+    public TeamController(ITeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+
+
+    }
 
     @GetMapping(value = "getall")
     public Iterable<Team> getall() {
-        logger.info("Finde den Fehler");
+        logger.info("Getall request successful");
         return teamRepository.findAll();
     }
 
 
     @GetMapping(value = "get")
     public ResponseEntity<Team> delete(@RequestBody String json) {
-        TeamID teamID = new Gson().fromJson(json, Team.class);
-        Optional<Team> team = teamRepository.findById(teamId);
-        Logger logger;
+        TeamID teamID = new Gson().fromJson(json, TeamID.class);
+        Optional<Team> team = teamRepository.findById(teamID);
         if (team.isEmpty()) {
-            logger.info("schonwieder ein Fehler:{}", teamID);
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND;
+            logger.info("Get request not successful for:{}", teamID);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         logger.info("Get request sucsessful for: {}", teamID);
         return new ResponseEntity<>(team.get(), HttpStatus.OK);
