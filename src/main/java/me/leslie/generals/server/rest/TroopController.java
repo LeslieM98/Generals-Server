564d31f0768cbb1ad5e232d6d021f0b7c1b37d47
leslie <2007732+LeslieM98@users.noreply.github.com>
2020-04-28 02:23:09 +0200

    removed businesslogic from TroopController

--- src/main/java/me/leslie/generals/server/rest/TroopController.java
+++ src/main/java/me/leslie/generals/server/rest/TroopController.java
@@ -1,10 +1,8 @@
 package me.leslie.generals.server.rest;
 
 import com.google.gson.Gson;
-import me.leslie.generals.server.model.gameentity.Army;
 import me.leslie.generals.server.model.gameentity.Troop;
-import me.leslie.generals.server.repository.gameentity.IArmyRepository;
-import me.leslie.generals.server.repository.gameentity.ITroopRepository;
+import me.leslie.generals.server.service.TroopService;
 import me.leslie.generals.server.valueobject.TroopID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,79 +11,66 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.NonNull;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/troop/*")
 public class TroopController {
-    private final ITroopRepository troopRepository;
-    private final IArmyRepository armyRepository;
+    private final TroopService troopService;
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
-    public TroopController(ITroopRepository troopRepository, IArmyRepository armyRepository) {
-        this.troopRepository = Objects.requireNonNull(troopRepository, "troopRepository is null");
-        this.armyRepository = Objects.requireNonNull(armyRepository, "armyRepository is null");
-    }
-
-    private void deleteTroopFromArmy(TroopID troopID, IArmyRepository armyRepository) {
-        List<Army> armies = new ArrayList<>();
-        armyRepository.findAll().forEach(armies::add);
-
-        for (var army : armies) {
-            if (army.getTroopIDS().contains(troopID)) {
-                Set<TroopID> updatedTroops = new HashSet<>(army.getTroopIDS());
-                updatedTroops.remove(troopID);
-                Army updatedArmy = new Army(army.getHq(), updatedTroops);
-                armyRepository.save(updatedArmy);
-            }
-        }
+    public TroopController(@NonNull TroopService troopService) {
+        this.troopService = troopService;
     }
 
     @PutMapping(value = "save")
     public ResponseEntity<String> save(@RequestBody String json) {
         Troop troop = new Gson().fromJson(json, Troop.class);
-        troopRepository.save(troop);
+        troopService.save(troop);
         logger.info("Save request for: {}", troop);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping(value = "delete")
     public ResponseEntity<Troop> delete(@RequestBody String json) {
-        TroopID troopID = new Gson().fromJson(json, TroopID.class);
-        Optional<Troop> troop = troopRepository.findById(troopID);
-        if (troop.isEmpty()) {
-            logger.info("Delete request not successful for: {}", troopID);
+        try {
+            TroopID troopID = new Gson().fromJson(json, TroopID.class);
+            Troop troop = troopService.delete(troopID);
+            logger.info("Delete request successful for: {}", troopID);
+            return new ResponseEntity<>(troop, HttpStatus.OK);
+        } catch (MissingResourceException e) {
+            logger.error("Delete request failed.", e);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        troopRepository.deleteById(troopID);
-        deleteTroopFromArmy(troopID, armyRepository);
-        logger.info("Delete request successful for: {}", troopID);
-        return new ResponseEntity<>(troop.get(), HttpStatus.OK);
     }
 
     @GetMapping(value = "getall")
-    public Iterable<Troop> getAll() {
+    public List<Troop> getAll() {
         logger.info("Getall request successful");
-        return troopRepository.findAll();
+        return troopService.getAll();
     }
 
     @GetMapping(value = "get")
     public ResponseEntity<Troop> get(@RequestParam int id) {
-        TroopID troopID = new TroopID(id);
-        Optional<Troop> troop = troopRepository.findById(troopID);
-        if (troop.isEmpty()) {
-            logger.info("Get request not successful for: {}", troopID);
+        try {
+            TroopID troopID = new TroopID(id);
+            Troop troop = troopService.get(troopID);
+            logger.info("Get request successful for: {}", troopID);
+            return new ResponseEntity<>(troop, HttpStatus.OK);
+        } catch (MissingResourceException e) {
+            logger.error("Get request not successful", e);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        logger.info("Get request successful for: {}", troopID);
-        return new ResponseEntity<>(troop.get(), HttpStatus.OK);
     }
 
     @GetMapping(value = "getallids")
     public ResponseEntity<List<Integer>> getAllIDs() {
-        ResponseEntity<List<Integer>> response = new ResponseEntity<>(troopRepository.findAllTroopIDs().stream().map(TroopID::getValue).collect(Collectors.toList()), HttpStatus.OK);
+        ResponseEntity<List<Integer>> response = new ResponseEntity<>(
+                troopService.getAllIDs().stream().map(TroopID::getValue).collect(Collectors.toList()), HttpStatus.OK);
         logger.info("GetAllIDs request successful");
         return response;
     }
